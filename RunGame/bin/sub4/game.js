745813b3d646f55e365fb7fd2992d3c55b66fadf
Rabbit <529450239@qq.com>
2021-03-09 13:32:13 +0800

    修改子包

--- RunGame/bin/sub4/game.js
+++ RunGame/bin/sub4/game.js
@@ -1,1 +1,8 @@
 window.loadLib = require;
+loadLib("laya.core.min.js")
+    loadLib("laya.ani.min.js")
+    loadLib("laya.ui.min.js")
+    loadLib("laya.d3.min.js")
+    loadLib("laya.physics3D.min.js")
+    //-----libs-end-------
+    loadLib("bundle.js");
